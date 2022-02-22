e019b8e4a90352dacb3a6e5327ffb8516eb312fe
Rafsanjami-botmasrer <87937071+Rafsanjami-botmasrer@users.noreply.github.com>
2022-02-22 21:49:00 +0600

    Add files via upload

--- snake_game.py
+++ snake_game.py
@@ -1,9 +1,7 @@
-from tkinter import font
+#from tkinter import font
 import turtle
 import time
 import random
-
-from numpy import count_nonzero
 
 delay = 0.1
 
@@ -23,6 +21,12 @@ head.color("black")
 head.penup()
 head.goto(0,0)
 head.direction = "stop"
+
+#for segment in segments:
+   # segment.goto(1000, 1000)
+
+#segments.clear()
+
 
 food = turtle.Turtle()
 food.speed(0)
@@ -60,6 +64,9 @@ def go_right():
 
 def stop():
     head.direction = "stop"
+
+
+
 
 
 
@@ -101,9 +108,16 @@ while True:
         new_segments = turtle.Turtle()
         new_segments.speed(0)
         new_segments.shape("square")
-        new_segments.color("red")
+        new_segments.color("blue")
         new_segments.penup()
         segments.append(new_segments)
+
+        score += 10
+        if score > high_score:
+            high_score = score
+
+        pen.clear()
+        pen.write("Score: {} High Score: {}" .format(score, high_score), align="center", font=("Courier", 24, "normal"))
 
 
     for index in range(len(segments)-1, 0, -1):
@@ -117,6 +131,17 @@ while True:
         segments[0].goto(x, y)
 
     move()
+
+    for segment in segment:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+
+            for segment in segment:
+             segment.goto(1000, 1000)
+
+            segments.clear()
      
 
     time.sleep(delay)
